6db7d7f553ae3d6a40b3b8d7fac62e72409d7b68
Liza Grinenko <xli3kox@gmail.com>
2021-05-23 16:47:38 +0300

    chnage 11ty configuration

--- .eleventy.js
+++ .eleventy.js
@@ -3,17 +3,32 @@ module.exports = function (eleventyConfig) {
     // eleventyConfig.addFilter( "myFilter", function() {});
 
     // You can return your Config object (optional).
+
+    eleventyConfig.addPassthroughCopy('./src/static')
+    eleventyConfig.addWatchTarget('./src/static')
+
+    eleventyConfig.addNunjucksFilter("rusDate", function(value) {
+        const date = new Date(value)
+        const str = `${(date.getDate() + '').padStart(2,0)}.${(date.getMonth() + '').padStart(2,0)}.${date.getFullYear()}`
+        console.log(str)
+        return str
+    });
+
+    eleventyConfig.addNunjucksFilter("rusTime", function(value) {
+        return value.split(' ').join(':')
+    });
     return {
         dir: {
-            layouts: "templates",
-            markdown: "content"
+            input: 'src',
+            output: 'build'
+            // markdown: "content"
         },
 
-        dataTemplateEngine: 'njk',
-        markdownTemplateEngine: 'njk',
-        htmlTemplateEngine: 'njk',
-        templateFormats: [
-            'md', 'njk'
-        ],
+        // dataTemplateEngine: 'njk',
+        // markdownTemplateEngine: 'njk',
+        // htmlTemplateEngine: 'njk',
+        // templateFormats: [
+        //     'md', 'njk'
+        // ],
     };
 };
