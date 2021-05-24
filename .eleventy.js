3fbbb88ee8a330b765887ccca5da1df448c68612
Liza Grinenko <xli3kox@gmail.com>
2021-05-24 17:38:19 +0300

    event page finished

--- .eleventy.js
+++ .eleventy.js
@@ -1,26 +1,47 @@
+const Intl = require("intl")
 module.exports = function (eleventyConfig) {
-    // Add a filter using the Config API
-    // eleventyConfig.addFilter( "myFilter", function() {});
-
-    // You can return your Config object (optional).
 
     eleventyConfig.addPassthroughCopy('./src/static')
     eleventyConfig.addWatchTarget('./src/static')
 
-    eleventyConfig.addNunjucksFilter("rusDate", function(value) {
+    eleventyConfig.addNunjucksFilter("rusDate", function(value) { 
         const date = new Date(value)
-        const str = `${(date.getDate() + '').padStart(2,0)}.${(date.getMonth() + '').padStart(2,0)}.${date.getFullYear()}`
-        console.log(str)
-        return str
+        const formatOptions = {day: 'numeric', month: 'long', year: 'numeric', timeZone: 'UTC'}
+        const dateFormater = new Intl.DateTimeFormat('ru', formatOptions);
+        console.log('Intl.DateTimeFormat(date)', dateFormater.format(date).slice(0, -3)) 
+        
+        return dateFormater.format(date).slice(0, -3)
     });
 
     eleventyConfig.addNunjucksFilter("rusTime", function(value) {
         return value.split(' ').join(':')
     });
+
+    eleventyConfig.addNunjucksFilter("findByName", function(arr, name) {
+        console.log(name)
+        return arr.find(item => item.template.frontMatter.data.name === name)
+    });
+
+    eleventyConfig.addNunjucksFilter("findByNameArray", function(arr, addToFilter) {
+        return arr.filter(item => addToFilter.includes(item.template.frontMatter.data.name))
+    });
+
+    eleventyConfig.addFilter("dropContentFolder", function (path) {
+        if (path.endsWith("/index")) {
+            path = path.substring(0, -6);
+        }
+        const pathToDrop = "/data"
+        if (!path.startsWith(pathToDrop)) {
+            return path
+        }
+        return path.slice(pathToDrop.length)
+    })
+
     return {
         dir: {
             input: 'src',
-            output: 'build'
+            output: 'build',
+            // includes: '/src/_includes'
             // markdown: "content"
         },
 
--- src/static/event.js
+++ src/static/event.js
@@ -1,11 +1,3 @@
-// $(document).ready(function () {
-//     $("#ex1").modal({
-//         fadeDuration: 1000,
-//         fadeDelay: 0.50,
-//         showClose: false
-//     });
-// });
-
 
 const afisha = document.querySelector('.afisha__afisha');
 const afishaModal = document.querySelector('.modal');
@@ -22,6 +14,39 @@ afishaModal.addEventListener('click', e => {
         afishaModal.classList.remove('modal_open')
     }
 })
+
+const fullDescriptionContainer = document.querySelector('.afisha__description-container')
+const fullDescription = fullDescriptionContainer.querySelector('.afisha__full-description')
+const fullDescriptionContent = fullDescription.querySelector('p')
+const extendContentControl = fullDescriptionContainer.querySelector('.afisha__control')
+
+
+if(shouldBeVisible(fullDescription, fullDescriptionContent)) {
+    extendContentControl.addEventListener('click', () => {
+        fullDescriptionContainer.classList.toggle('afisha__description-container_short')
+        const textElement = extendContentControl.querySelector('.afisha__control-text')
+        
+        changeText(textElement, 'alternativeText')
+    })
+} else {
+    document.querySelector('.afisha__description-controls').classList.add('afisha__description-controls_hidden')
+}
+
+
+function shouldBeVisible(container, content) {
+    console.log(container,content)
+    const computedStyles = window.getComputedStyle(container)
+    const cleanHeight = container.clientHeight - parseInt(computedStyles.getPropertyValue("padding-top")) - parseInt(computedStyles.getPropertyValue("padding-bottom"))
+    console.log(cleanHeight, content.clientHeight)
+    
+    return cleanHeight < content.clientHeight;
+}
+
+function changeText(elem, attr) {
+    const newText = elem.getAttribute(attr)
+    elem.setAttribute(attr, elem.textContent)
+    elem.textContent = newText;
+}
 
 // closeAfishaModalButton.addEventListener('click', () => {
 //     afishaModal.classList.remove('modal_open')
